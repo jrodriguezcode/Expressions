0b10109bb4d5eb93ce0e91d37155088183c48ccf
jrodriguezcode <jr698751@gmail.com>
2025-02-03 13:55:55 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * expression = operands & operators
+		 * operands = values, variables, numbers, quantity
+		 * operators = + - * / %
+		 */
+		
+		int friends = 10;
+		
+		//friends += 1;
+		//friends -= 1;
+		
+		//friends *= 2;
+		//friends /= 2;
+		
+		//friends %= 3;
+		//friends %= 2;
+		
+		//friends++;
+		//friends--;
+		
+		System.out.println(friends);
+
+	}
+
+}
